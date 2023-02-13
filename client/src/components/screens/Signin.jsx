41490a04253688746c0203538084c8ae01ff83f7
aydink88 <aydink88@gmail.com>
2023-02-13 16:45:13 +0300

    demo login added

--- client/src/components/screens/Signin.jsx
+++ client/src/components/screens/Signin.jsx
@@ -1,45 +1,45 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import isEmail from "../../utils/isEmail";
-import { signinRequest } from "../../services/api";
-import { useUser } from "../../contexts/User";
-import { useUIContext } from "../../contexts/UIContext";
-import { Button, Card, Form } from "react-bootstrap";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import isEmail from '../../utils/isEmail';
+import { signinRequest } from '../../services/api';
+import { useUser } from '../../contexts/User';
+import { useUIContext } from '../../contexts/UIContext';
+import { Button, Card, Form } from 'react-bootstrap';
 
 const SignIn = () => {
   const { setToast } = useUIContext();
   const { dispatch } = useUser();
   const navigate = useNavigate();
-  const [password, setPasword] = useState("");
-  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   const submitSigninForm = async (e) => {
     e.preventDefault();
     if (!isEmail(email)) {
-      setToast("invalid email", "error");
+      setToast('invalid email', 'error');
       return;
     }
     if (!password) {
-      setToast("fill the fields", "error");
+      setToast('fill the fields', 'error');
       return;
     }
 
     try {
       const data = await signinRequest(email, password);
 
-      localStorage.setItem("jwt", data.token);
-      dispatch({ type: "USER", payload: data.user });
-      localStorage.setItem("user", JSON.stringify(data.user));
-      setToast("signedin success", "success");
-      navigate("/");
+      localStorage.setItem('jwt', data.token);
+      dispatch({ type: 'USER', payload: data.user });
+      localStorage.setItem('user', JSON.stringify(data.user));
+      setToast('signedin success', 'success');
+      navigate('/');
     } catch (err) {
-      setToast(err.message, "error");
+      setToast(err.message, 'error');
     }
   };
 
   return (
     <div className="d-flex justify-content-center">
-      <Card className="p-5 text-center my-3" style={{ maxWidth: "400px" }}>
+      <Card className="p-5 text-center my-3" style={{ maxWidth: '400px' }}>
         <Form onSubmit={submitSigninForm}>
           <Card.Header>
             <h2>Instagram</h2>
@@ -56,11 +56,22 @@ const SignIn = () => {
             type="password"
             placeholder="password"
             value={password}
-            onChange={(e) => setPasword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <Button type="submit" className="btn">
             Login
           </Button>
+          <Form.Check
+            className="my-4 text-start"
+            type="checkbox"
+            label="Login as demo user"
+            onChange={(e) => {
+              if (e.target.checked) {
+                setEmail('demo@demo.com');
+                setPassword('demo1234');
+              }
+            }}
+          />
         </Form>
         <Card.Footer className="my-4">
           <h5>
